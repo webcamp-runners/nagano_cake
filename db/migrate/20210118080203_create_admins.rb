8ff3b3112375208b083df37d9b00aa857e30b5a5
seeya1127 <seeya11271995@gmail.com>
2021-01-18 11:22:05 +0000

    fix model

--- db/migrate/20210118080203_create_admins.rb
+++ db/migrate/20210118080203_create_admins.rb
@@ -2,7 +2,7 @@ class CreateAdmins < ActiveRecord::Migration[5.2]
   def change
     create_table :admins do |t|
       t.string :email
-      t.tring :encrypted_password
+      t.string :encrypted_password
 
       t.timestamps
     end
